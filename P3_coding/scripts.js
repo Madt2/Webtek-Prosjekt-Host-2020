b2ea1127d3d2a4e829632eae959b5338d89cd679
mklevs99 <matsi99@live.com>
2020-10-29 16:46:07 +0100

    la til js- kommentar utifra tidspunktet på dagen

--- P3_coding/scripts.js
+++ P3_coding/scripts.js
@@ -58,3 +58,20 @@ function header_and_footer() {
 }
 
 
+function fyllaKommentar() {
+    var time = new Date().getHours();  // Henter inn tidspunktet på dagen 
+    if (time > 6 && time < 14) {
+        document.getElementById("demo").innerHTML = "Kjør dagsfylla?";  // Kommentar som vises når klokken er mellom 06:00 og 14:00
+    }
+    
+    else if (time > 14 && time < 18) {
+        document.getElementById("demo").innerHTML = "PS: Du har et vors å rekke!"; // Kommentar som vises når klokken er mellom 14:00 og 18:00
+    }
+    
+    else if (time < 6 && time > 18) {
+        document.getElementById("demo").innerHTML = "Håper du i dette øyeblikk nyter en hjemmebrygget enhet!"; // Kommentar som vises når klokken er mellom 18:00 og 06:00
+    }
+    
+}
+
+fyllaKommentar()
