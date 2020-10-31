7d9994b4ed1c99654fb742de8d1cb5cbcc32f1e0
Auk-Debug <emil.jar@live.no>
2020-10-31 18:04:59 +0100

    endret på kommentarer i alkohol_kalk.js

--- Ontap/scripts/Alkohol_kalk.js
+++ Ontap/scripts/Alkohol_kalk.js
@@ -11,7 +11,7 @@ btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// Når brukeren trykker på X i span elementet vil popupen lukkes(ikke vises)
+// Når brukeren trykker på span elementet vil popupen lukkes(ikke vises)
 span.onclick = function() {
   modal.style.display = "none";
 }
@@ -24,15 +24,15 @@ window.onclick = function(event) {
 }
 
 //Kode for å regne ut alkoholvolum i prosent
-document.getElementById('alk_prosent_button').addEventListener("click", function alk_i_prosent(){ //Legger til funksjonen til "Alkohol i volum" knappen
-    var output = document.getElementById('alk_prosent'); //Henter output element fra oppskrifter.html
-   const OG_verdi = document.getElementById('OG_value').value; //Henter input verdi fra bruker for Original Gravity
-   const FG_verdi = document.getElementById('FG_value').value; //Henter input verdi fra bruker for Final Gravity
-   if (OG_verdi < FG_verdi) {                                  //Sjekker om Final Gravity er større enn Original Gravity da dette ikke funker for formelen
+document.getElementById('alk_prosent_button').addEventListener("click", function alk_i_prosent(){ // Legger til funksjonen til "Alkohol i volum" knappen
+    var output = document.getElementById('alk_prosent'); // Henter output element fra oppskrifter.html
+   const OG_verdi = document.getElementById('OG_value').value; // Henter input verdi fra bruker for Original Gravity
+   const FG_verdi = document.getElementById('FG_value').value; // Henter input verdi fra bruker for Final Gravity
+   if (OG_verdi < FG_verdi) {                                  // Sjekker om Final Gravity er større enn Original Gravity da dette ikke funker for formelen
        output.value = "FG verdi for stor"
    }
 
    else {
-       output.value = ((OG_verdi - FG_verdi) * 105 * 1.25).toPrecision(3) + "%"; //Formel fått fra klient, kode vil regne ut alkohol i volum utifra oppgitt Original Gravity og Final Gravity
+       output.value = ((OG_verdi - FG_verdi) * 105 * 1.25).toPrecision(3) + "%"; // Formel fått fra klient, kode vil regne ut alkohol i volum utifra oppgitt Original Gravity og Final Gravity
    }
 });
