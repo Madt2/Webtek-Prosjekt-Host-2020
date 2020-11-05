2936978a2df83f880311cd00af15540e3384ed24
Tor Madsen <tormads@stud.ntnu.no>
2020-11-05 01:27:38 +0100

    Fikset kommentarer på galleri.html, picture_view.js og style.css'

--- Ontap/scripts/picture_view.js
+++ Ontap/scripts/picture_view.js
@@ -12,16 +12,17 @@ function closeModalbox() {
 var slideIndex = 1;
 showSlides(slideIndex);
 
+// funksjon som setter slideIndex til n
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-// Funksjon for å gå til neste bilde eller forrige bilde. I HTML-en n satt til -1 når plusSlides(-1) og 1 når plusSlides(1). 
+// Funksjon for å gå til neste bilde eller forrige bilde. I HTML-en n satt til -1 når plusSlides(-1) og 1 når plusSlides(1)
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Hvis brukeren klikker utenfor modal elementet lukkes popupen
+// Hvis brukeren klikker utenfor modal elementet lukkes popup-en
 var modal = document.getElementById("currentModalbox");
 window.onclick = function(event) {
   if (event.target == modal) {
@@ -29,17 +30,16 @@ window.onclick = function(event) {
   }
 }
 
-// Funksjon for å gjøre at om man går tilbake når slideIndex = 1 skal den bli 10, og om man går frem og slideIndex = 10 skal den bli 1
-// brukes også for 
+// Funksjon for å skjule og vise bildene i visningsmodus
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
+  var i; // definerer verdien i
+  var slides = document.getElementsByClassName("slides"); // lager listen "slides" som inneholder elementene fra classen "slides"
 
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (n > slides.length) {slideIndex = 1} // gjør at om man trykker neste bilde og slideIndex = 10 skal den bli 1
+  if (n < 1) {slideIndex = slides.length} // gjør at om man trykker forrige bilde og slideIndex = 1 skal den bli 10
+  for (i = 0; i < slides.length; i++) { // for løkke som skjuler alle slides/bildene (setter display lik "none" for alle elementene i listen "slides")
       slides[i].style.display = "none";
   }
   
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block"; // viser sliden/bildet som tilsvarer slideIndexen (det må være slideIndex-1 siden listen begynner å telle fra 0 mens indexene i html-en begynner på 1)
 }
